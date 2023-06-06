56df66d92395479899a0706b18e6fb62d2d4498d
petrostrak <pit.trak@gmail.com>
2023-06-06 23:24:18 +0300

    Add unique name and optional description

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "companies" (
   "id" uuid DEFAULT gen_random_uuid(),
-  "name" varchar(15) NOT NULL,
-  "description" varchar(3000) NOT NULL,  
+  "name" varchar(15) NOT NULL UNIQUE,
+  "description" varchar(3000) NULL,  
   "number_of_employees" integer NOT NULL,
   "registered" boolean NOT NULL,
   "type" varchar NOT NULL,
